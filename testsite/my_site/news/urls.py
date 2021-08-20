c4b4653624946e3306c0d624449c8d065248c040
supru <suprunmax1981@gmail.com>
2021-08-21 02:18:33 +0300

    Replaced functions in views with ListView classes

--- testsite/my_site/news/urls.py
+++ testsite/my_site/news/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('category/<int:category_id>/', get_category, name='category'),
+    path('category/<int:category_id>/', GetCategory.as_view(), name='category'),
     path('news/<int:news_id>/', view_news, name='view-news'),
     path('news/add_news/', add_news, name='add-news'),
-    path('', index, name='home'),
+    path('', HomeNewsPage.as_view(), name='home'),
+    # path('', index, name='home'),
 ]
--- testsite/my_site/news/views.py
+++ testsite/my_site/news/views.py
@@ -1,27 +1,56 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import News, Category
 from .forms import NewsForm
+from django.views.generic import ListView
 
 
-def index(request):
-    published_news = list()
-    news = News.objects.all()
-    for new in range(len(news)):
-        if news[new].is_published:
-            published_news.extend({news[new]})
-    context = {'news': published_news, 'title': 'Список новостей'}
-    return render(request, template_name='news/index.html', context=context)
+class HomeNewsPage(ListView):
+    model = News
+    template_name = 'news/index.html'
+    context_object_name = 'news'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Главная страница'
+        return context
+
+    def get_queryset(self):
+        return News.objects.filter(is_published=True)
 
 
-def get_category(request, category_id):
-    published_news = list()
-    news = News.objects.filter(category_id=category_id)
-    for new in range(len(news)):
-        if news[new].is_published:
-            published_news.extend({news[new]})
-    category = Category.objects.get(pk=category_id)
-    context = {'news': published_news, 'category': category}
-    return render(request, template_name='news/category.html', context=context)
+# def index(request):
+# published_news = list()
+#    news = News.objects.filter(is_published=True)
+# for new in range(len(news)):
+#    if news[new].is_published:
+#        published_news.extend({news[new]})
+#    context = {'news': news, 'title': 'Список новостей'}
+#    return render(request, template_name='news/index.html', context=context)
+
+class GetCategory(ListView):
+    model = News
+    template_name = 'news/category.html'
+    context_object_name = 'news'
+    allow_empty = False
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(GetCategory, self).get_context_data(**kwargs)
+        context['category_title'] = Category.objects.get(pk=self.kwargs['category_id'])
+        return context
+
+    def get_queryset(self):
+        return News.objects.filter(category_id=self.kwargs['category_id'], is_published=True)
+
+
+# def get_category(request, category_id):
+# published_news = list()
+#    news = News.objects.filter(category_id=category_id, is_published=True)
+# for new in range(len(news)):
+#    if news[new].is_published:
+#       published_news.extend({news[new]})
+#    category = Category.objects.get(pk=category_id)
+#    context = {'news': news, 'category': category}
+#    return render(request, template_name='news/category.html', context=context)
 
 
 def view_news(request, news_id):
